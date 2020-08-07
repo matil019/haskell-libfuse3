b200e0590cdeb5fb0fd538611edca759f8d50f49
matil019 <yohashi1991@gmail.com>
2020-08-08 00:05:19 +0900

    Remove unnecessary parameters from null.hs

--- null.hs
+++ null.hs
@@ -35,9 +35,9 @@ nullGetattr "/" = do
     }
 nullGetattr _ = pure $ Left eNOENT
 
-nullTruncate :: FilePath -> FileOffset -> IO Errno
-nullTruncate "/" _ = pure eOK
-nullTruncate _   _ = pure eNOENT
+nullTruncate :: FilePath -> IO Errno
+nullTruncate "/" = pure eOK
+nullTruncate _   = pure eNOENT
 
 nullOpen :: FilePath -> IO (Either Errno ())
 nullOpen "/" = pure $ Right ()
@@ -49,17 +49,17 @@ nullRead "/" size offset
   | otherwise = pure $ Right $ B.replicate (fromIntegral size) 0
 nullRead _ _ _ = pure $ Left eNOENT
 
-nullWrite :: FilePath -> ByteString -> FileOffset -> IO (Either Errno CInt)
-nullWrite "/" buf _ = pure $ Right $ fromIntegral $ B.length buf
-nullWrite _   _   _ = pure $ Left eNOENT
+nullWrite :: FilePath -> ByteString -> IO (Either Errno CInt)
+nullWrite "/" buf = pure $ Right $ fromIntegral $ B.length buf
+nullWrite _   _   = pure $ Left eNOENT
 
 main :: IO ()
 main = fuseMain ops (\e -> hPrint stderr (e :: SomeException) >> pure eIO)
   where
   ops = defaultFuseOps
     { Fuse.fuseGetattr = Just $ \fp _ -> nullGetattr fp
-    , Fuse.fuseTruncate = Just $ \fp _ off -> nullTruncate fp off
+    , Fuse.fuseTruncate = Just $ \fp _ _ -> nullTruncate fp
     , Fuse.fuseOpen = Just $ \fp _ _ -> nullOpen fp
     , Fuse.fuseRead = Just $ \fp _ sz off -> nullRead fp sz off
-    , Fuse.fuseWrite = Just $ \fp _ buf off -> nullWrite fp buf off
+    , Fuse.fuseWrite = Just $ \fp _ buf _ -> nullWrite fp buf
     }
