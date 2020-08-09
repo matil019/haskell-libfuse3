338d4f31f0d4845fdc50d68015c8f97504144a12
matil019 <yohashi1991@gmail.com>
2020-08-09 17:03:11 +0900

    Add daemonizeResourceT
    Forks and transfers the ownerships of the resources to the child

--- src/System/Fuse3/Internal/Resource.hs
+++ src/System/Fuse3/Internal/Resource.hs
@@ -2,10 +2,32 @@
 module System.Fuse3.Internal.Resource where
 
 -- TODO no all-in imports
+import Control.Exception
 import Control.Monad.Trans.Resource
 import Control.Monad.Trans.Resource.Internal
 import Foreign
 import Foreign.C
+import System.Posix.Process
+
+import Control.Monad.IO.Class (liftIO)
+import System.Exit (ExitCode(ExitSuccess))
+
+-- | Forks a new process and transfers the resources to it.
+--
+-- The parent process `exitImmediately`.
+daemonizeResourceT :: ResourceT IO a -> ResourceT IO b
+daemonizeResourceT res = do
+  -- We don't use resourceForkWith because we don't want to increase refcounts
+  istate <- getInternalState
+  liftIO $ do
+    _ <- forkProcess $ mask $ \restore -> do
+      _ <- restore (runInternalState res istate) `catch` \e -> do
+        stateCleanupChecked (Just e) istate
+        throwIO e
+      stateCleanupChecked Nothing istate
+    -- cleanup actions are discarded because the child will run them
+    exitImmediately ExitSuccess
+    undefined
 
 resCallocBytes :: Int -> ResourceT IO (ReleaseKey, Ptr a)
 resCallocBytes = _
