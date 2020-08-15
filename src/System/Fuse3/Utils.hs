d5e8e810f274d5c67f432336edb9e3ab9d6024e0
matil019 <yohashi1991@gmail.com>
2020-08-16 01:41:31 +0900

    Organize the documentation of System.Fuse3.Utils

--- src/System/Fuse3/Utils.hs
+++ src/System/Fuse3/Utils.hs
@@ -1,37 +1,53 @@
 -- | Miscellaneous utilities provided for convenience.
 --
 -- These can be used for general purpose and are not directly related to FUSE.
-module System.Fuse3.Utils where
+module System.Fuse3.Utils
+  ( -- * Bitsets
+    testBitSet
+
+  , -- * Errno
+    unErrno, ioErrorToErrno, tryErrno, tryErrno_
+
+  , -- * Marshalling strings
+    pokeCStringLen0
+
+  , -- * TimeSpec
+    timeSpecToPOSIXTime
+  )
+  where
 
 import Control.Exception (tryJust)
 import Data.Bits ((.&.), Bits)
 import Data.Ratio ((%))
 import Data.Time.Clock.POSIX (POSIXTime)
 import Foreign (copyArray, pokeElemOff)
-import Foreign.C (CInt, CStringLen, Errno(Errno), eOK, withCStringLen)
+import Foreign.C (CInt, CStringLen, Errno(Errno), eOK, errnoToIOError, throwErrno, withCStringLen)
 import GHC.IO.Exception (IOException(IOError, ioe_errno))
 import System.Clock (TimeSpec)
 
 import qualified System.Clock as TimeSpec
 
+-- to have haddock link to proper entities
+_dummy :: dummy
+_dummy = error "dummy" errnoToIOError throwErrno
+
 -- | Unwraps the newtype `Errno`.
 unErrno :: Errno -> CInt
 unErrno (Errno errno) = errno
 
--- | Attempts to extract an `Errno` from an `IOError` assuming it is
+-- | Attempts to extract an `Errno` from an t`IOError` assuming it is
 -- constructed with `errnoToIOError` (typically via `throwErrno`).
 ioErrorToErrno :: IOError -> Maybe Errno
 ioErrorToErrno IOError{ioe_errno=Just e} = Just $ Errno e
 ioErrorToErrno _ = Nothing
 
--- | Catches an exception constructed with `errnoToIOError`.
+-- | Catches an exception constructed with `errnoToIOError` and extracts `Errno` from it.
 tryErrno :: IO a -> IO (Either Errno a)
 tryErrno = tryJust ioErrorToErrno
 
--- | Catches an exception constructed with `errnoToIOError`, discarding the result of the
--- original action.
+-- | Like `tryErrno` but discards the result of the original action.
 --
--- If the original action doesn't throw an error, returns `eOK`.
+-- If no exceptions, returns `eOK`.
 tryErrno_ :: IO a -> IO Errno
 tryErrno_ = fmap (either id (const eOK)) . tryErrno
 
@@ -45,8 +61,10 @@ timeSpecToPOSIXTime ts = fromRational $ TimeSpec.toNanoSecs ts % 10^(9::Int)
 --
 -- Does `withCStringLen` and copies it into the destination buffer.
 --
--- If the destination buffer is not long enough to hold the source string, it is truncated and a
--- NUL byte is appended at the end of the buffer.
+-- The Haskell string should not contain NUL characters.
+--
+-- If the destination buffer is not long enough to hold the source string, it is truncated
+-- and a NUL byte is inserted at the end of the buffer.
 pokeCStringLen0 :: CStringLen -> String -> IO ()
 pokeCStringLen0 (pBuf, bufSize) src =
   withCStringLen src $ \(pSrc, srcSize) -> do
