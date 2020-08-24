d9075cd0fc13d459eeb53841cd228d3b30f5c9f2
matil019 <yohashi1991@gmail.com>
2020-08-25 00:26:40 +0900

    Document example/statjson.hs with minor improvements

--- example/statjson.hs
+++ example/statjson.hs
@@ -2,6 +2,44 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+-- |
+-- Copyright: (c) 2020 yohashi
+-- License  : MIT
+--
+-- A read-only filesystem that exposes file stats in JSON files.
+--
+-- This filesystem mirrors the content of a source directory along with JSON files generated
+-- next to each file. JSON files have \".stat.json\" at the end of their names.
+--
+-- For example:
+--
+-- Source directory:
+--
+-- > a.jpg
+-- > b
+-- > b/c.txt
+--
+-- Mounted directory:
+--
+-- > rootdir.stat.json  <- struct stat of the source directory
+-- > a.jpg
+-- > a.jpg.stat.json    <- struct stat of a.jpg
+-- > b
+-- > b.stat.json        <- struct stat of b
+-- > b/c.txt
+-- > b/c.txt.stat.json  <- struct stat of b/c.txt
+--
+-- If the source directory contains files which already have \".stat.json\" at the end of
+-- their names, they are shadowed (but their own JSON files are still generated, so you'll
+-- see \".stat.json.stat.json\" files).
+--
+-- This example demonstrates the expressiveness of Haskell. It uses the \"layered\"
+-- approach. There are three parts in this program; the part that generates
+-- \"rootdir.stat.json\", another for generating \".stat.json\" for each file, and the other
+-- that provides a read-only view of the source directory. For each of the parts a set of
+-- filesystem operations is defined as a layer. Operations may process and respond to
+-- filesystem requests themselves or delegate to the next layer. Finally the layers are
+-- combined to a @FuseOperations@.
 module Main(main) where
 
 import Data.Aeson ((.=))
@@ -18,6 +56,7 @@ import System.FilePath ((<.>), dropTrailingPathSeparator, stripExtension)
 import System.LibFuse3
 import System.IO (hPrint, stderr)
 import System.Posix.IO (OpenFileFlags, OpenMode(ReadOnly), closeFd, openFd)
+import System.Posix.Files (regularFileMode, stdFileMode)
 import System.Posix.Types (COff(COff), CSsize(CSsize), ByteCount, Fd(Fd), FileOffset)
 
 import qualified Data.Aeson as Aeson
@@ -28,13 +67,23 @@ import qualified System.Clock
 foreign import ccall "pread"
   c_pread :: CInt -> Ptr a -> CSize -> COff -> IO CSsize
 
-data Outcome a = Delegate | Respond a | Merge a (a -> a -> a)
+-- | An outcome of an individual operation in a layer.
+data Outcome a
+  -- | The request is forwarded to the next layer.
+  = Delegate
+  -- | The request is handled by the operation.
+  | Respond a
+  -- | The request is handled by both the operation and the next layer, and the results are
+  -- merged by the function.
+  | Merge a (a -> a -> a)
 
 type Action a = IO (Either Errno (Outcome a))
 
+-- | A set of operations, or a layer.
 data OpsLayer fh = OpsLayer
   { layerGetAttr  :: FilePath -> Action FileStat
   , layerReaddir  :: FilePath -> Action [(String, Maybe FileStat)]
+  -- | `Merge` is treated as same as `Respond`
   , layerOpen     :: FilePath -> OpenMode -> OpenFileFlags -> Action fh
   , layerRead     :: fh -> ByteCount -> FileOffset -> IO (Either Errno ByteString)
   , layerRelease  :: fh -> IO ()
@@ -45,6 +94,7 @@ removeWriteModes stat = stat { fileMode = go (fileMode stat) }
   where
   go mode = mode .&. complement 0o222
 
+-- | Provides the read-only view of the source directory.
 passthroughOps :: FilePath -> OpsLayer Fd
 passthroughOps srcDir = OpsLayer
   -- note that we can't use (</>) to concatenate to srcDir because paths passed from libfuse start with '/'
@@ -64,6 +114,8 @@ passthroughOps srcDir = OpsLayer
   , layerRelease = closeFd
   }
 
+-- | Presents the \"\/roodir.stat.json\" file whose content is the metadata of the source
+-- directory.
 generateRootStatJsonOps :: FilePath -> OpsLayer FileStat
 generateRootStatJsonOps srcDir = OpsLayer
   { layerGetAttr = \path ->
@@ -97,6 +149,7 @@ generateRootStatJsonOps srcDir = OpsLayer
   , layerRelease = \_ -> pure ()
   }
 
+-- | Encodes a `FileStat` into a JSON data.
 generateFileStatJson :: FileStat -> BL.ByteString
 generateFileStatJson stat = Aeson.encode $ Aeson.object
   [ "st_ino"    .= toInt (fileID stat)
@@ -115,6 +168,7 @@ generateFileStatJson stat = Aeson.encode $ Aeson.object
   toInt x = fromIntegral x :: Int
   timeSpecToJson TimeSpec{sec, nsec} = Aeson.object [ "sec" .= sec, "nsec" .= nsec ]
 
+-- | Presents a \".stat.json\" file for each file in the source directory.
 generateFileStatJsonOps :: FilePath -> OpsLayer FileStat
 generateFileStatJsonOps srcDir = OpsLayer
   { layerGetAttr = \path ->
@@ -165,6 +219,9 @@ andThen io f = io >>= \case
   Left e -> pure $ Left e
   Right a -> f a
 
+-- | Chains two `Action`s into one.
+--
+-- Runs the first `Action` and then run the second if needed.
 chainActions :: Action a -> Action a -> Action a
 chainActions a1 a2 = a1 `andThen` \case
   Respond a  -> pure $ Right $ Respond a
@@ -177,6 +234,9 @@ chainActions a1 a2 = a1 `andThen` \case
 
 infixl 2 `chainActions`
 
+-- | Runs (possibly a chain of) `Action`(s).
+--
+-- If it returns `Merge`, it is treated as `Respond`.
 runAction :: Action a -> IO (Either Errno a)
 runAction = fmap $ \case
   Left e -> Left e
@@ -184,36 +244,60 @@ runAction = fmap $ \case
   Right (Respond a) -> Right a
   Right (Merge a _) -> Right a
 
-ops :: FilePath -> FuseOperations (Either (Either FileStat FileStat) Fd) Void
+data TheHandle
+  = GenRootStatHandle FileStat
+  | GenFileStatHandle FileStat
+  | PassthroughHandle Fd
+
+-- | Merges the three layers into one, and converts it to `FuseOperations`.
+ops :: FilePath -> FuseOperations TheHandle Void
 ops srcDir = defaultFuseOperations
-  { fuseGetattr = Just $ \path _ -> runAction $
+  { -- return the response of the first layer to respond.
+    fuseGetattr = Just $ \path _ -> runAction $
       layerGetAttr (generateRootStatJsonOps  srcDir) path `chainActions`
       layerGetAttr (generateFileStatJsonOps srcDir) path `chainActions`
       layerGetAttr (passthroughOps          srcDir) path
-  , fuseReaddir = Just $ \path _ -> runAction $
+  , -- return the response of the first layer to respond.
+    fuseReaddir = Just $ \path _ -> runAction $
       layerReaddir (generateRootStatJsonOps  srcDir) path `chainActions`
       layerReaddir (generateFileStatJsonOps srcDir) path `chainActions`
       layerReaddir (passthroughOps          srcDir) path
-  , fuseOpen = Just $ \path mode flags ->
+  , -- try each layers in the order and put the result into the 3-way handle.
+    -- here we treat `Merge` as if `Respond`.
+    fuseOpen = Just $ \path mode flags ->
       layerOpen (generateRootStatJsonOps srcDir) path mode flags `andThen` \case
-        Respond x -> pure $ Right $ Left $ Left x
-        Merge x _ -> pure $ Right $ Left $ Left x
+        Respond x -> pure $ Right $ GenRootStatHandle x
+        Merge x _ -> pure $ Right $ GenRootStatHandle x
         Delegate -> layerOpen (generateFileStatJsonOps srcDir) path mode flags `andThen` \case
-          Respond x -> pure $ Right $ Left $ Right x
-          Merge x _ -> pure $ Right $ Left $ Right x
+          Respond x -> pure $ Right $ GenFileStatHandle x
+          Merge x _ -> pure $ Right $ GenFileStatHandle x
           Delegate -> layerOpen (passthroughOps srcDir) path mode flags `andThen` \case
-            Respond x -> pure $ Right $ Right x
-            Merge x _ -> pure $ Right $ Right x
+            Respond x -> pure $ Right $ PassthroughHandle x
+            Merge x _ -> pure $ Right $ PassthroughHandle x
             Delegate -> pure $ Left eNOSYS
-  , fuseRead    = Just $ \_ -> either3 (layerRead    (generateRootStatJsonOps srcDir)) (layerRead    (generateFileStatJsonOps srcDir)) (layerRead    (passthroughOps srcDir))
-  , fuseRelease = Just $ \_ -> either3 (layerRelease (generateRootStatJsonOps srcDir)) (layerRelease (generateFileStatJsonOps srcDir)) (layerRelease (passthroughOps srcDir))
+  , -- branch on the 3-way handle created by fuseOpen.
+    fuseRead = Just $ \_ -> \case
+      GenRootStatHandle stat -> layerRead (generateRootStatJsonOps srcDir) stat
+      GenFileStatHandle stat -> layerRead (generateFileStatJsonOps srcDir) stat
+      PassthroughHandle fd   -> layerRead (passthroughOps srcDir) fd
+  , -- branch on the 3-way handle created by fuseOpen.
+    fuseRelease = Just $ \_ -> \case
+      GenRootStatHandle stat -> layerRelease (generateRootStatJsonOps srcDir) stat
+      GenFileStatHandle stat -> layerRelease (generateFileStatJsonOps srcDir) stat
+      PassthroughHandle fd   -> layerRelease (passthroughOps srcDir) fd
   }
-  where
-  either3 :: (a -> r) -> (b -> r) -> (c -> r) -> Either (Either a b) c -> r
-  either3 fa fb fc = either (either fa fb) fc
 
+-- |
+--
+-- In addition to the common commandline arguments of libfuse applications, this accepts
+-- @--source PATH@ which takes the source directory.
 main :: IO ()
 main = do
-  (srcDir':rest) <- getArgs
+  (srcDir', rest) <- do
+    args <- getArgs
+    case break (=="--source") args of
+      (pre, _source:path:suf) -> pure (path, pre <> suf)
+      (_, _source:[]) -> fail "`--source` option expects a path"
+      (_, []) -> fail "requires an option `--source PATH`"
   srcDir <- dropTrailingPathSeparator <$> makeAbsolute srcDir'
   withArgs rest $ fuseMain (ops srcDir) (\e -> hPrint stderr (e :: IOError) >> pure eIO)
