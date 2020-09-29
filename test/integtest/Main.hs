05e6e4c8226261842e4b2034899dc344899da8fe
matil019 <yohashi1991@gmail.com>
2020-09-29 21:57:10 +0900

    Use assertEqual in getattrTest

--- test/integtest/Main.hs
+++ test/integtest/Main.hs
@@ -2,7 +2,6 @@ module Main where
 
 import Control.Exception (SomeException, finally)
 import Data.Bits ((.|.))
-import Data.Void (Void)
 import Foreign.C (eIO, eNOENT)
 import System.Environment (withArgs)
 import System.FilePath ((</>))
@@ -25,17 +24,17 @@ runTestCase :: TestCase fh dh -> IO ()
 runTestCase TestCase{testCaseOps=ops, testCaseProg=prog} =
   withSystemTempDirectory "libfuse3test" $ \mountPoint -> do
     let unmount = do
-          hPutStrLn stderr $ "unmounting: " <> mountPoint
+          hPutStrLn stderr $ "unmounting : " <> mountPoint
           callProcess "fusermount3" ["-u", mountPoint]
     withArgs [mountPoint] $ do
-      hPutStrLn stderr $ "mounting a test filesystem on: " <> mountPoint
+      hPutStrLn stderr $ "mounting on: " <> mountPoint
       pid <- forkProcess $ fuseMain ops (\e -> hPrint stderr (e :: SomeException) >> pure eIO)
       flip finally unmount $ do
         -- wait for fuseMain to daemonize
         _ <- getProcessStatus True False pid
         prog mountPoint
 
-getattrTest :: TestCase Void Void
+getattrTest :: TestCase fh dh
 getattrTest =
   let stat = defaultFileStat
         { fileMode = directoryMode .|. 0o644 -- TODO 0o755
@@ -54,7 +53,7 @@ getattrTest =
         -- ignore differences in fileID
         -- TODO test use_ino?
         let stat'' = stat' { fileID = fileID stat }
-        print $ stat == stat''
+        assertEqual "getattrTest" stat stat''
   in TestCase ops prog
 
 -- | If `fuseOpen` is not defined, make sure that `fuseRead` doesn't throw unless
